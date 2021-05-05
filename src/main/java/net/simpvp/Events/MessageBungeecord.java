fcf8770ff1de9875e87a94ae30492c9135fe123a
MoWobbler <64741387+MoWobbler@users.noreply.github.com>
2021-05-05 11:07:54 -0400

    Update MessageBungeecord.java

--- src/main/java/net/simpvp/Events/MessageBungeecord.java
+++ src/main/java/net/simpvp/Events/MessageBungeecord.java
@@ -1,35 +1,17 @@
 package net.simpvp.Events;
 
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import org.bukkit.plugin.messaging.PluginMessageListener;
-
-import com.google.common.io.ByteArrayDataInput;
+import com.google.common.collect.Iterables;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 
-import net.md_5.bungee.api.ChatColor;
 
+public class MessageBungeecord {
 
-public class MessageBungeecord implements PluginMessageListener{
-	@Override
-	public void onPluginMessageReceived(String channel, Player player, byte[] message) {
-	if (!channel.equals("BungeeCord")) {
-	  return;
-	}
-	ByteArrayDataInput in = ByteStreams.newDataInput(message);
-	String subchannel = in.readUTF();
-	if (subchannel.equals("PlayerList")) {	  
-		  String[] playerList = in.readUTF().split(", ");
-		  messagePlayer(playerList,player,EaCommand.getMessage());
-
-	    }
-	  }
-	
+	//Connect player to a server
 	public static void connect(Player player, String server) {
 		ByteArrayDataOutput output = ByteStreams.newDataOutput();
 		output.writeUTF("Connect");
@@ -38,40 +20,13 @@ public class MessageBungeecord implements PluginMessageListener{
 		player.sendPluginMessage(Events.instance,"BungeeCord",output.toByteArray());
 	}
 	
-	
-	 /* First method called by the broadcast command
-	 *  Sends a request to the bungeecord server
-	 *  This method triggers the onPluginMessageReceived event
-	 */
-	public static void PlayerList(Player player) {
+	//Message all players
+	public static void messageAll(String msg) {
 		ByteArrayDataOutput out = ByteStreams.newDataOutput();
-		  out.writeUTF("PlayerList");
+		  out.writeUTF("Message");
 		  out.writeUTF("ALL");
+		  out.writeUTF(msg);
+		  Player player = Iterables.getFirst(Bukkit.getOnlinePlayers(), null);
 		  player.sendPluginMessage(Events.instance, "BungeeCord", out.toByteArray());
 	}
-	
-	 /* This method sends a message to all players
-	 */
-	public static void messagePlayer(String[] listOfPlayers,Player sender, String msg) {
-		for (String s: listOfPlayers) {
-			 if (s != null) {		  
-					ByteArrayOutputStream b = new ByteArrayOutputStream();
-					DataOutputStream out = new DataOutputStream(b);
-					
-					try {
-						out.writeUTF("Message");
-						out.writeUTF(s);
-						Events.instance.getLogger().info(s);
-						out.writeUTF(ChatColor.LIGHT_PURPLE + msg);
-						
-					}catch(Exception e) {
-						e.printStackTrace();
-					}
-					sender.sendPluginMessage(Events.instance, "BungeeCord", b.toByteArray());	  
-	    		  }
-	    	  }
-	}
-	
-
-
 }
